081945b2afc71ff089cc99264a93133d6d6430b2
miald <miladalikhani76@gmail.com>
2016-03-18 23:37:23 +0330

    make validation package and make TestMain class

---
+++ src/Validation/TestMain.java
@@ -0,0 +1,12 @@
+package Validation;
+
+/**
+ * Created by milad on 3/18/16.
+ */
+
+public class TestMain {
+    public static void main(String[] args) {
+        int a = 10 ;
+        System.out.println(a);
+    }
+}
