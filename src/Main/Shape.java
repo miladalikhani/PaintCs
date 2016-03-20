da8231fc96805a8aeadd4c6742c29999f95ee8f3
irphysicist <m.darestani98@gmail.com>
2016-03-20 16:27:37 +0330

    1.01

--- src/Main/Shape.java
+++ src/Main/Shape.java
@@ -1,5 +1,56 @@
 package Main;
 
-abstract public class Shape {
+import java.awt.*;
 
+public abstract class Shape {
+    protected Point Location;
+    protected Color Border;
+    protected Color Fill;
+    protected double A, B;
+    protected String Name;
+    protected double Angle;
+
+    public Shape (String name, Point location, Color border, Color fill) {
+        this.Name = name;
+        this.Location = location;
+        this.Border = border;
+        this.Fill = fill;
+        this.A = 0;
+        this.B = 0;
+        this.Angle = 0;
+    }
+
+    public Color getBorder() { return Border; }
+
+    public void setBorder (Color border) { Border = border; }
+
+    public Color getFill() { return Fill; }
+
+    public void setFill (Color fill) { Fill = fill; }
+
+    public double getA() { return A; }
+
+    public void setA (double a) { A = a; }
+
+    public double getAngle() { return Angle; }
+
+    public void setAngle (double angle) { Angle = angle; }
+
+    public double getB() { return B; }
+
+    public void setB (double b) { B = b; }
+
+    public Point getLocation() { return Location; }
+
+    public void setLocation (Point location) { Location = location; }
+
+    public String getName() { return Name; }
+
+    public void setName (String name) { Name = name; }
+
+    public abstract void Move (Point newLocation);
+    public abstract void Scale (double k);
+    public abstract void ChangeBorder (Color newBorder);
+    public abstract void ChangeFill (Color newFill);
+    public abstract void Rotate (double angle);
 }
